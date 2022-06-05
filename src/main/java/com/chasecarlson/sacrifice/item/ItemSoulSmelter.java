aab86b321ff1bc0193e6b19d7cea3becd8b6c781
Chase Carlson <chasecarlson08@gmail.com>
2022-06-05 16:48:54 -0700

    Changed soul smelter behavior

--- src/main/java/com/chasecarlson/sacrifice/item/ItemSoulSmelter.java
+++ src/main/java/com/chasecarlson/sacrifice/item/ItemSoulSmelter.java
@@ -6,9 +6,13 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
@@ -31,13 +35,13 @@ public class ItemSoulSmelter extends Item {
 	@Override
 	public InteractionResult useOn(UseOnContext ctx) {
 		Level level = ctx.getLevel();
-		Sacrifice.LOGGER.info("Soul Smelter USED");
+		Sacrifice.LOGGER.debug("Soul Smelter USED");
 		ItemStack soulSmelterItem = ctx.getItemInHand();
 		BlockPos pos = ctx.getClickedPos();
 		Block blockClicked = level.getBlockState(pos).getBlock();
 		Item blockItem = blockClicked.asItem();
 		ItemStack blockItemStack = new ItemStack(blockItem);
-		Sacrifice.LOGGER.info("Soul Smelter used on "+blockClicked.getRegistryName());
+		Sacrifice.LOGGER.debug("Soul Smelter used on "+blockClicked.getRegistryName());
 
 
 		ItemStack smeltedItemStack = level.getRecipeManager().getRecipeFor(RecipeType.SMELTING, new SimpleContainer(blockItemStack), level)
@@ -48,19 +52,38 @@ public class ItemSoulSmelter extends Item {
 		if (smeltedItemStack != null) {
 			if (soulSmelterItem.getDamageValue() < this.getMaxDamage(soulSmelterItem)) {
 				if (!level.isClientSide()) {
-					level.setBlock(pos, Blocks.AIR.defaultBlockState(), 3);
-					level.updateNeighborsAt(pos, Blocks.AIR);
-					ItemEntity itemOnGround = new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, smeltedItemStack, 0, 0.2, 0);
-					level.addFreshEntity(itemOnGround);
+					if (smeltedItemStack.getItem() instanceof BlockItem)
+					{
+						BlockItem smeltedItemBlock = (BlockItem) smeltedItemStack.getItem();
+						Block smeltedBlock = smeltedItemBlock.getBlock();
+						level.setBlock(pos, smeltedBlock.defaultBlockState(), 3);
+						level.updateNeighborsAt(pos, smeltedBlock);
+					}
+					else
+					{
+						level.setBlock(pos, Blocks.AIR.defaultBlockState(), 3);
+						level.updateNeighborsAt(pos, Blocks.AIR);
+						ItemEntity itemOnGround = new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, smeltedItemStack, 0, 0.2, 0);
+						level.addFreshEntity(itemOnGround);
+						return InteractionResult.SUCCESS;
+					}
 					soulSmelterItem.setDamageValue(soulSmelterItem.getDamageValue() + 1);
-					Sacrifice.LOGGER.info("Soul Smelter functions");
-					return InteractionResult.SUCCESS;
 				}
 				level.playSound(null, pos, new SoundEvent(new ResourceLocation("block.fire.ambient")), SoundSource.AMBIENT, 1.0f, 2.0f);
 			}
 			else {
 				level.playSound(null, pos, new SoundEvent(new ResourceLocation("block.dispenser.fail")), SoundSource.AMBIENT, 1.0f, 1.5f);
 			}
+		}
+		return InteractionResult.PASS;
+	}
+
+	@Override
+	public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity entity, InteractionHand hand) {
+		if (!entity.isOnFire()) {
+			entity.setSecondsOnFire(10);
+			stack.setDamageValue(stack.getDamageValue() + 1);
+			return InteractionResult.SUCCESS;
 		}
 		return InteractionResult.PASS;
 	}
